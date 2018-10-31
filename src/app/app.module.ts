e9ac0db7ed4abaa93277b9b2b4e912ad3da04f41
Yevgeny Levin <devlev1980@gmail.com>
2018-10-31 21:27:16 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeFormComponent } from './home-form/home-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeFormComponent
   ],
   imports: [
     BrowserModule,
